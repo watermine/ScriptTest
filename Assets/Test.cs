7bbf491c559c72efe8fc1820a34100e82ef5c7f9
mizuki.mineo <minewater@live.jp>
2023-07-20 16:19:18 +0900

    配列名pointsをarrayへ変更
    課題内容に合うよう、配列名pointsをarrayへ変更した

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -33,20 +33,20 @@ public class Test : MonoBehaviour
     void Start()
     {
         //要素数5の配列を初期化し、各要素に値を代入する
-        int[] points = { 1, 2, 3, 4, 5 };
+        int[] array = { 1, 2, 3, 4, 5 };
 
         //配列の要素数のぶんだけ昇順で処理を繰り返す
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0; i < array.Length; i++)
         {
             //配列の要素をすべて表示する
-            Debug.Log(points[i]);
+            Debug.Log(array[i]);
         }
 
         //配列の要素数のぶんだけ降順で処理を繰り返す
         for (int i = 4; i >= 0; i--)
         {
             //配列の要素をすべて表示する
-            Debug.Log(points[i]);
+            Debug.Log(array[i]);
 
         }
 
